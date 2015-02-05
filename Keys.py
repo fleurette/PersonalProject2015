cf8501e1576519ced83489084dc62e551d982e9f
Pierre Sutter <pierre.sutter@gmail.com>
2015-02-05 17:41:04 +0000

    Init

---
+++ Keys.py
@@ -0,0 +1,4 @@
+consumerKey = "41Nq7mtmT0DxMrY5yjpBH0b6a"
+consumerSecret = "2qvk6Qkd4wcLGx72QhIj1gKPfgK0BPuu3cCl3KU3sSl1uq8P6i"
+accessToken = "2976517792-kZ7JzXAHH0xww3DAlpDYAT71x0CkmcvKBMlFEeR"
+accessSecret = "Uv16uH2l8AEL1ywEdUZAxd6II7K9CsqteOHKGYH0kwId6"
---
+++ Test.py
@@ -0,0 +1,9 @@
+import tweepy
+
+from Data import *
+from Keys import *
+
+test = tweepy.api.user_timeline("imp_proj_01")
+print test
+
+
