7d0d31ac0103d4b383ea0029c3bbe6cf2d4e6a4a
Pierre Sutter <pierre.sutter@gmail.com>
2015-02-05 19:18:46 +0000

    First commit

--- Test.py
+++ Test.py
@@ -3,7 +3,12 @@ import tweepy
 from Data import *
 from Keys import *
 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
 test = tweepy.api.user_timeline("imp_proj_01")
-print test
+print 42
 
 
