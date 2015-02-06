4a9272d20be576b02ebbcb1294ea30ecb02dd95a
Pierre Sutter <pierre.sutter@gmail.com>
2015-02-06 12:12:56 +0000

    Ready for testing phase

--- src/DBManagement.py
+++ src/DBManagement.py
@@ -1,10 +1,17 @@
 from pymongo import MongoClient
 
-def initialiseDB():
+def initialiseDB(Credentials):
   # Read DBCredential file
-  with open('DBCredentials.dat') as f:
+  with open(Credentials) as f:
     content = f.read().splitlines()
   
   client = MongoClient(content[3], int(content[2]))
+  db = client.test
   
-  return client
+  return db.collection
+
+def writeUsersDB(db, user, tweets):
+  data = user.__dict__
+  data['tweets'] = tweets.__dict__
+
+  db.insert(data)
