c848b86672431e6e1fa7df9cd45a7073509e5916
Pierre <pierre.sutter11@imperial.ac.uk>
2015-02-17 11:04:07 +0000

    Update APIManagement.py
    Hid access codes

--- src/crawler/APIManagement.py
+++ src/crawler/APIManagement.py
@@ -1,9 +1,9 @@
 import tweepy
 
-consumerKey = "41Nq7mtmT0DxMrY5yjpBH0b6a"
-consumerSecret = "2qvk6Qkd4wcLGx72QhIj1gKPfgK0BPuu3cCl3KU3sSl1uq8P6i"
-accessKey = "2976517792-kZ7JzXAHH0xww3DAlpDYAT71x0CkmcvKBMlFEeR"
-accessSecret = "Uv16uH2l8AEL1ywEdUZAxd6II7K9CsqteOHKGYH0kwId6"
+consumerKey = ""
+consumerSecret = ""
+accessKey = ""
+accessSecret = ""
 
 def getAPI():
   auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
